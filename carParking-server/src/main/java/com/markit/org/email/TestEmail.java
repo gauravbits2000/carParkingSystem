03e53e465d8eafbead7d8791b501443a1d543bdf
Hari Abdesh Gupta <abdesh.hari@gmailcom>
2018-10-09 11:55:21 +0530

    Server Changes - Email Class

---
+++ carParking-server/src/main/java/com/markit/org/email/TestEmail.java
@@ -0,0 +1,41 @@
+package com.markit.org.email;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import com.markit.org.email.EmailSender.EmailStatus;
+
+
+
+public class TestEmail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sendEmail();
+
+	}
+
+	/**
+	 * 
+	 */
+	public static void sendEmail() {
+		  String mailTo = "hari.gupta@ihsmarkit.com,gaurav.agarwal@ihsmarkit.com,parag.garg@ihsmarkit.com";
+		EmailSender emailSender = new EmailSender(mailTo);
+
+		
+		emailSender.setEmailStatus(EmailStatus.SUCCESS);
+		emailSender.setSubject("Parking Allocation Lucky Draw");
+		String hostName = "";
+		try {
+			hostName = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException ex) {
+		}
+
+		emailSender.appendContent(EmailStatus.SUCCESS.toString() + " : Host " + hostName);
+		emailSender.newLine();
+
+		emailSender.appendContent("Parking Details");
+
+		emailSender.sendEmail();
+	}
+
+}
