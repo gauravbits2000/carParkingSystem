ecc1719d37f099babb5eb90c17bb654792d3a399
Gaurav Agarwal <gauravbits2000@gmail.com>
2018-10-14 01:44:26 +0530

    Complete Animation

--- carParking-web/src/main/web/src/app/lottrey/lottrey.component.ts
+++ carParking-web/src/main/web/src/app/lottrey/lottrey.component.ts
@@ -13,7 +13,7 @@ export class LottreyComponent implements OnInit {
 
   employeeList : Employee[];
   resultTable: Employee[];
-  //resultTempTable: Employee[];
+ // resultTempTable: Employee[];
   imgPath:string;
   imgPath1:string;
   
@@ -34,7 +34,7 @@ export class LottreyComponent implements OnInit {
   paginators1: Array<any> = [];
   
   
-  /*  resultTable = [
+  /*employeeList = [
     new Employee("11", "Abc", "abc@gmail.com", "DL 5C 6789"),
     new Employee("22", "Abc1", "abc@gmail.com", "DL 5C 6789"),
     new Employee("34", "Abc2", "abc@gmail.com", "DL 5C 6789"),
@@ -44,20 +44,29 @@ export class LottreyComponent implements OnInit {
     new Employee("25", "Abc4", "abc@gmail.com", "DL 5C 6789")    
   ];*/
 
-  resultTempTable = [
+  /*resultTable = [
+    new Employee("11", "Abc", "abc@gmail.com", "DL 5C 6789"),
+    new Employee("22", "Abc1", "abc@gmail.com", "DL 5C 6789"),
+    new Employee("34", "Abc2", "abc@gmail.com", "DL 5C 6789"),
+    new Employee("41", "Abc3", "abc@gmail.com", "DL 5C 6789"),
+    new Employee("15", "Abc4", "abc@gmail.com", "DL 5C 6789"),
+     new Employee("15", "Abc4", "abc@gmail.com", "DL 5C 6789"),
+    new Employee("25", "Abc4", "abc@gmail.com", "DL 5C 6789")   
+
+  ] ;*/
+
+    resultTempTable = [
     new Employee("11", "Abc", "abc@gmail.com", "DL 5C 6789"),
     new Employee("22", "Abc1", "abc@gmail.com", "DL 5C 6789"),
     new Employee("34", "Abc2", "abc@gmail.com", "DL 5C 6789"),
     new Employee("41", "Abc3", "abc@gmail.com", "DL 5C 6789"),
     new Employee("15", "Abc4", "abc@gmail.com", "DL 5C 6789"),
 
-  ]
-  
-  
+  ]  
   
   constructor(private employeeService: EmployeeService, private el: ElementRef) { }
 
-changePage(event: any) {
+changePage(event: any) {  
     if (event.target.text >= 1 && event.target.text <= this.numberOfPaginators) {
       this.activePage = +event.target.text;
       this.firstVisibleIndex = this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
@@ -65,7 +74,7 @@ changePage(event: any) {
     }
   }
 
-  nextPage(event: any) {
+  nextPage(event: any) {    
     if (this.pages.last.nativeElement.classList.contains('active')) {
       if ((this.numberOfPaginators - this.numberOfVisiblePaginators) >= this.lastVisiblePaginator) {
         this.firstVisiblePaginator += this.numberOfVisiblePaginators;
@@ -121,14 +130,13 @@ changePage(event: any) {
 
 
  	ngOnInit() 
- 	{
-
-    
+ 	{    
  	   this.employeeService.getEmployeeDetailsAll("http://localhost:8080/fetch-employees").subscribe((data: Employee[]) => 
 	 	   {
-	 	   console.log(data);
-	      this.employeeList = data;
-	        console.log(this.employeeList);
+	 	     console.log(data);
+	       this.employeeList = data;
+	       console.log(this.employeeList);
+        
     
 	    this.startDraw = false;
 	    if (this.employeeList.length % this.itemsPerPage === 0) {
@@ -136,10 +144,15 @@ changePage(event: any) {
 	    } else {
 	      this.numberOfPaginators = Math.floor(this.employeeList.length / this.itemsPerPage + 1);
 	    }
+
+       console.log(this.activePage);
 	
 	    console.log(this.employeeList);
 	    for (let i = 1; i <= this.numberOfPaginators; i++) {
 	      this.paginators.push(i);
+        console.log(this.paginators);
+
+
 	    }
 	       }, err => {
 	      console.log(err);
@@ -164,8 +177,7 @@ changePage(event: any) {
     var table2 = $(".card2");
     var table3 = $(".card3");
 
-   $('#final').css("display","none");
-  
+   $('#final').css("display","none");  
   
     let firstVisibleIndex = this.firstVisibleIndex;
     let lastVisibleIndex = this.lastVisibleIndex;
@@ -182,11 +194,12 @@ if (this.resultTable.length % this.itemsPerPage1 === 0) {
     console.log(this.resultTable);
     for (let i = 1; i <= this.numberOfPaginators1; i++) {
         this.paginators1.push(i);
+        console.log(this.paginators1);
     }
 
-
-      table1.addClass('inner-container-animate1').delay(1000).queue(function () {
-      table1.addClass('inner-container-animate2').delay(1000).queue(function () {
+      
+      table1.addClass('inner-container-animate1').delay(3000).queue(function () {
+      table1.addClass('inner-container-animate2').delay(3000).queue(function () {
       table2.addClass('inner-container-animate1');
       table2.addClass('inner-container-animate3');
 
@@ -204,7 +217,7 @@ if (this.resultTable.length % this.itemsPerPage1 === 0) {
             }(i));
           } 
 
-          setTimeout(()=>{        
+          setTimeout(function(){        
             table1.css({opacity:'0'},1000);
             //table1.fadeOut(1000);
             $('.inner-container1').fadeIn(2000);
@@ -218,13 +231,10 @@ if (this.resultTable.length % this.itemsPerPage1 === 0) {
             
             var pos1 = $('.table2 tr:nth-child(1) td:nth-child(2)').offset();
                      var pos2 = $('.table3 tr:nth-child(1) td:nth-child(2)').offset();
-                     var final_pos_top = (pos2.top-pos1.top) + pos1.top+7;
+                     var final_pos_top = (pos2.top-pos1.top) + pos1.top -75;
                      console.log(pos1.left);
                      console.log(pos2.left);
-                     
-                     
-                      var final_pos_left =  (pos1.left-pos2.left)+ pos1.left;
-                      
+                      var final_pos_left =  (pos1.left-pos2.left)+ pos1.left;                      
                       console.log(final_pos_left);
 
             for (var i = 1; i <= result_temp.length; i++) {
@@ -233,7 +243,7 @@ if (this.resultTable.length % this.itemsPerPage1 === 0) {
                   window.setTimeout(function () {   
                     //console.log($('.table3 tr:nth-child(' + i + ') td:nth-child(2)').html());
                   $('.table3 tr:nth-child(' + i + ') td:nth-child(2)').css({"position":"relative",'visibility':'visible'})
-                  .animate({bottom: '+=' +  final_pos_top + 'px'},1000).animate({left: '715px'},1000);
+                  .animate({bottom: '+=' +  final_pos_top + 'px'},1000).animate({left: '750px'},1000);
                 }, i * 1000);
               }(i));
             } 
@@ -241,23 +251,31 @@ if (this.resultTable.length % this.itemsPerPage1 === 0) {
             window.setTimeout(function(){
               $('img').animate({ opacity: '0' },"slow"); 
               this.isFinal = false;
+
               $('.table3 td:nth-child(2)').css({'visibility':'hidden'});
+              
 
               //$("#temp").css({"visibility":"hidden"});
                  //$("#final").css({"visibility":"visible"});
 
-                 $("#temp").fadeOut(2000);                 
+                 $("#temp").fadeOut(2000,function(){
+                   //$("#temp").css({"display":"none"});
+                    $('.table2 td:nth-child(1)').css({color:"black"});
+                $('.table2 td:nth-child(2)').css({color:"black"});
+                $('.table2 td:nth-child(3)').css({color:"black"});
+                $('.table2 td:nth-child(4)').css({color:"black"}); 
+                   
+                 });                 
 
-              $("#final").fadeIn(2000,function(){
+              $("#final").fadeIn(3000,function(){
+                //$("#final").css({"visibility":"visible"});
                 $('.table2 td:nth-child(1)').css({color:"black"});
                 $('.table2 td:nth-child(2)').css({color:"black"});
-                 $('.table2 td:nth-child(3)').css({color:"black"});
-              $('.table2 td:nth-child(4)').css({color:"black"});
+                $('.table2 td:nth-child(3)').css({color:"black"});
+                $('.table2 td:nth-child(4)').css({color:"black"}); 
+                $("#final").addClass('element-animation');
+              //$("#final").animate({width: '400px',height:'700px'},1000);
               });
-              
-              
-
-
               
             },result_temp.length*2000);
           },j*1500);          
@@ -276,6 +294,7 @@ if (this.resultTable.length % this.itemsPerPage1 === 0) {
       this.getTempLuckyDrawResult();
       console.log("Result" + this.resultTable);
       this.Start();
+      //this.startDraw = false;
     }, err => {
       console.log(err);
     })
